c7a88ea2b490063053af67deb5ec6d7ff7cf21f1
thinkySide <113565086+thinkySide@users.noreply.github.com>
2023-02-07 09:15:46 +0900

    Initial Commit

---
+++ CodeSnipet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeSnipet
+//
+//  Created by 김민준 on 2023/02/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
