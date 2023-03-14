ce64b038348184ac7bde49a53a7092d47bd2501d
thinkySide <113565086+thinkySide@users.noreply.github.com>
2023-03-14 11:15:28 +0900

    [Update]

---
+++ CodeSnippet/Snippet/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CodeSnipet
+//
+//  Created by 김민준 on 2023/02/07.
+//
+
+import UIKit
+
+// $uiVC
+final class ViewController: UIViewController {
+    
+    // MARK: - Variable
+    private let myView = View()
+    
+    
+    
+    // MARK: - ViewController LifeCycle
+    override func loadView() {
+        view = myView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupInitial()
+    }
+    
+    
+    
+    // MARK: - Method
+    func setupInitial() {
+        // 기본 구현
+    }
+}
